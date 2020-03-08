db1f6a1c2fd6d5c20f0d3e771a95c383363fecc1
Quang Huy Trần <traquahu1997@gmail.com>
2020-03-08 23:10:20 +0700

    add Configuration cho entity

---
+++ EShopSolution.Data/Configurations/AppConfigConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using EShopSolution.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShopSolution.Data.Configurations
+{
+    public class AppConfigConfiguration : IEntityTypeConfiguration<AppConfig>
+    {
+        public void Configure(EntityTypeBuilder<AppConfig> builder)
+        {
+            builder.ToTable("AppConfigs");
+            builder.HasKey(i => i.Key);
+            builder.Property(i => i.Value).IsRequired(true);
+        }
+    }
+}
--- EShopSolution.Data/EF/EShopDbContext.cs
+++ EShopSolution.Data/EF/EShopDbContext.cs
@@ -1,4 +1,5 @@
-﻿using EShopSolution.Data.Entity;
+﻿using EShopSolution.Data.Configurations;
+using EShopSolution.Data.Entity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,24 @@ namespace EShopSolution.Data.EF
         {
             
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CartConfiguration());
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new ContactConfiguration());
+            modelBuilder.ApplyConfiguration(new LanguageConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new PromotionConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
 
