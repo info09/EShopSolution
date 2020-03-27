ca915b938ea874972067793f7380a9c06cc6d823
Quang Huy Trần <traquahu1997@gmail.com>
2020-03-27 23:09:49 +0700

    commit

---
+++ EShopSolution.ViewModels/System/User/LoginRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShopSolution.ViewModels.System.User
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(i => i.UserName).NotEmpty().WithMessage("Tài khoản không được để trống");
+            RuleFor(i => i.Password).NotEmpty().WithMessage("Mật khẩu không được để trống")
+                                    .MinimumLength(6).WithMessage("Mật khẩu tối thiểu 6 kí tự");
+        }
+    }
+}
